e67aa872dc89eee57823b3a1b06a81cb1c26e752
Abhijit <abhijit.chowdhury10192@gmail.com>
2016-05-26 18:12:12 +0530

    Thumbnail available for single fie upload. Will handle thumbnails for multiple on a separate branch

--- app/scripts/fileUpload.js
+++ app/scripts/fileUpload.js
@@ -1,34 +1,53 @@
 function generateThumbnail(node){
 	
- 	var files   = node.files;
- 	console.log(files); 	 	  	
- 	var parent  = $(node).parent();
+ 	var files   	= node.files;
+ 	//console.log(files); 	 	  	
+ 	var parent  	= $(node).parent();
 	var grandparent = parent.parent();
-	var clone 	=	grandparent.clone();
-	clone.filter(':eq(0)').children().empty().append('<canvas id="imageCanvas"></canvas>');
-	var reader  = new FileReader();
+	var clone 		= grandparent.clone();
+	clone.filter(':eq(0)').children().children().remove();	
+	
+	
 	for (var i = files.length - 1; i >= 0; i--) {
 		console.log('loop entered');
+		clone.filter(':eq(0)').children().append('<canvas id="imageCanvas' + i + '" width="75" height="75"></canvas>');
+		console.log(clone);
+		clone.insertAfter(grandparent); 
+		var canvas  	= document.getElementById('imageCanvas' + i);
+		var ctx 		= canvas.getContext('2d');	        		  
+		var reader    = new FileReader();
 		reader.onload = function(event){
 			console.log('reader loaded');	 	
-	 		clone.insertAfter(grandparent); 
-	        var img = new Image();
-	        img.onload = function(){	        	
-	            $('imageCanvas').width = 20;
-	            $('imageCanvas').height = 20;
-	            $('imageCanvas').getContext('2d').drawImage(img,0,0);        
-	        	img.src = event.target.result;
-	    	}
-	    	
-	    	reader.readAsDataURL(files[i]);
-	    	
 	 		
-	 		console.log(clone);
+	        var img 	= new Image();
+	        img.onload 	= function(){
+	        	// draw image into the canvas
+	        	console.log('img loaded');	        	
+	        	
+	        	//canvas.height	= canvas.width * (img.height / img.width);
+	        	var oc 			= document.createElement('canvas'),
+	        	octx 			= oc.getContext('2d');
 
+	        	oc.width 		= img.width * 0.5;
+	        	oc.height 		= img.height * 0.5;
+	        	octx.drawImage(img, 0, 0, oc.width, oc.height);
 
+	        	
+	        	octx.drawImage(oc, 0, 0, oc.width * 0.5, oc.height * 0.5);
 
-	 		
+	        	ctx.drawImage(oc, 0, 0, oc.width * 0.5, oc.height * 0.5);      	
+
+        	 	            
+	    	};
+
+	    	// set image src to reader content	
+	    	img.src = event.target.result;		 		
+	 			 		
 	 	};
+
+	 	console.log(files[i]);
+	 	// read file and return content as a URL	
+	 	reader.readAsDataURL(files[i]);
  	
 	};
   	
