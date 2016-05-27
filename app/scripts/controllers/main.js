d1da002947cfd7a81f8882854fd16ac830f257ad
Abhijit <abhijit.chowdhury10192@gmail.com>
2016-05-27 17:59:37 +0530

    - generateThumbnail moved to a new directive 'thumbnail' - propertyImage service created Current status- injector error

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,9 +8,14 @@
  * Controller of the happyBuyingApp
  */
 angular.module('happyBuyingApp')
-  .controller('MainCtrl', function ($scope,$routeParams,properties,Upload) {
+  .controller('MainCtrl', function ($scope,$routeParams,properties,propertyImages) {
     $scope.property = {};
-
+    
+    $scope.saveFile = function(){
+        if ($scope.property.image) {
+           propertyImages.images.push($scope.property.image);            
+        };
+    };
     //array of features for checkboxes
     $scope.features = ["Air conditioning", "Balcony", "Bedding", "Cable TV", "Cleaning after exit", "Cofee pot", 
                                   "Computer", "Cot", "Dishwasher", "DVD", "Fan", "Fridge", "Grill", "Hairdryer", "Heating", 
@@ -19,32 +24,15 @@ angular.module('happyBuyingApp')
                                   "Use of pool"];
 
 
+
+
      //save property call                               
      $scope.saveProperty = function(property){
         console.log(property);
         properties.saveProperty(property);
      };
 
-     // upload multiple images on selection
-     $scope.policy= 'ewogICJleHBpcmF0aW9uIjogIjIwMjAtMDEtMDFUMDA6MDA6MDBaIiwKICAiY29uZGl0aW9ucyI6IFsKICAgIHsiYnVja2V0IjogImhhcHB5YnV5aW5nLmltYWdlcyJ9LAogICAgWyJzdGFydHMtd2l0aCIsICIka2V5IiwgIiJdLAogICAgeyJhY2wiOiAicHJpdmF0ZSJ9LAogICAgWyJzdGFydHMtd2l0aCIsICIkQ29udGVudC1UeXBlIiwgIiJdLAogICAgWyJzdGFydHMtd2l0aCIsICIkZmlsZW5hbWUiLCAiIl0sCiAgICBbImNvbnRlbnQtbGVuZ3RoLXJhbmdlIiwgMCwgNTI0Mjg4MDAwXQogIF0KfQ==';
-     $scope.signature= 'Ua9AKCIrwbzI7H/hwb5kbhUt6eA=';
-     $scope.uploadFiles= function(files){
-        if (files && files.length) {
-          Upload.upload({
-            url: 'http://happybuying.images.s3.amazonaws.com/', //S3 upload url including bucket name
-            method: 'POST',            
-            data: {
-                key: files.name, // the key to store the file on S3, could be file name or customized
-                AWSAccessKeyId: 'AKIAJABKAUGPWPCJM44Q',                
-                policy: $scope.policy, // base64-encoded json policy (see article below)
-                signature: $scope.signature, // base64-encoded signature based on policy string (see article below)                                
-                files: files
-            }
-          }).success(function(data){
-              console.log(data);
-          });
-        };
-     };
+
 
   
 })
