c96d1ee70441a9abe7ad48f8657092c47bfad1c4
Abhijit <abhijit.chowdhury10192@gmail.com>
2016-05-27 15:42:11 +0530

    Thumbnails are now appended into a single div

--- app/scripts/fileUpload.js
+++ app/scripts/fileUpload.js
@@ -1,24 +1,16 @@
 function generateThumbnail(node){
 	
  	var files   	= node.files;
- 	//console.log(files); 	 	  	
- 	var parent  	= $(node).parent();
-	var grandparent = parent.parent();
-	var clone 		= grandparent.clone();
-	clone.filter(':eq(0)').children().children().remove();
-	clone.filter(':eq(0)').removeClass('col-sm-4').addClass('col-sm-8');	
-	
+ 	//console.log(files);
 	
 	for (var i = files.length - 1; i >= 0; i--) {
-		console.log('loop entered');
-		clone.filter(':eq(0)').children().append('<div id="imageBlock"></div>');
-		console.log(clone);
-		clone.insertAfter(grandparent); 
+		//console.log('loop entered');
+		//console.log(clone);		
 		var reader    = new FileReader();
 		// read file and return content as a URL	
 		reader.readAsDataURL(files[i]);
 		reader.onload = function(event){
-			console.log('reader loaded');	 		
+			//console.log('reader loaded');	 		
 	        var imgSrc = event.target.result;
 	        var img  = document.createElement('img');
 	        img.src = imgSrc;
@@ -27,7 +19,7 @@ function generateThumbnail(node){
 	 			 		
 	 	};
 
-	 	console.log(files[i]);
+	 	//console.log(files[i]);
 	 	
  	
 	};
