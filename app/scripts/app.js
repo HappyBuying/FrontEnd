54efa1b65f33b99a4abf11d89505466298621780
Abhijit <abhijit.chowdhury10192@gmail.com>
2016-06-24 17:18:45 +0530

    golu changes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,7 +16,8 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch',
-    "checklist-model"
+    'checklist-model',
+    'ngMap' 
   ])
   .config(function ($routeProvider) {
     $routeProvider
--- app/scripts/directives/fileUpload.js
+++ app/scripts/directives/fileUpload.js
@@ -6,39 +6,35 @@ angular.module('happyBuyingApp')
         link: function(scope, element) {
             element.bind('change', function(e) {
                 var files   	= e.target.files;
-                
-                //var scope = element.scope();
-                //scope.$apply(function(){
-                //scope.property.images = files;
-                //});               
-                	
-            	for (var i = files.length - 1; i >= 0; i--) {
-            		//console.log('loop entered');
-            		//console.log(clone);		
-            		propertyImages.images.push(files.item(i));
-                    var reader    = new FileReader();
-            		// read file and return content as a URL	
-            		reader.readAsDataURL(files[i]);
-            		reader.onload = function(event){
-            			//console.log('reader loaded');	 		
-            	        var imgSrc = event.target.result;
-                        var cell = document.createElement('div');
-            	        var img  = document.createElement('img');
-                        var icon = document.createElement('i');
-            	        img.src = imgSrc;
-            	        img.className = 'thumbnail';	       
-                        icon.className = 'fa fa-times-circle';
-                        cell.className = 'imgCell';
-                        cell.appendChild(img);
-                        cell.appendChild(icon);                         
-            	        document.getElementById('imageBlock').appendChild(cell);                        
-            	 		icon.onclick = function(){
-                            propertyImages.images.splice(i, 1);
-                            cell.style.display = "none";
-                            console.log(propertyImages.images);
-                            console.log(scope.property.images);
-                        }; 		
-            	 	};
+                if (files){                                
+                	for (var i = files.length - 1; i >= 0; i--) {
+                		//console.log('loop entered');
+                		//console.log(clone);	
+                        propertyImages.images.push(files.item(i));             	            		
+                        var reader    = new FileReader();
+                		// read file and return content as a URL	
+                		reader.readAsDataURL(files[i]);
+                		reader.onload = function(event){
+                			//console.log('reader loaded');	 		
+                	        var imgSrc = event.target.result;
+                            var cell = document.createElement('div');
+                	        var img  = document.createElement('img');
+                            var icon = document.createElement('i');
+                	        img.src = imgSrc;
+                	        img.className = 'thumbnail';	       
+                            icon.className = 'fa fa-times-circle';
+                            cell.className = 'imgCell';
+                            cell.appendChild(img);
+                            cell.appendChild(icon);                         
+                	        document.getElementById('imageBlock').appendChild(cell);                        
+                	 		icon.onclick = function(){
+                                propertyImages.images.splice(i, 1);
+                                cell.style.display = "none";
+                                console.log(propertyImages.images);
+                                console.log(scope.property.images);
+                            }; 		
+                	 	};
+                    }
                 }
 
                 console.log(propertyImages.images);
